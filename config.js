2bb221cdeeb79b0d085c8fe906ac726e3ab594b7
Jackson <87233635+JacksonTheProgrammer@users.noreply.github.com>
2021-12-01 13:19:34 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,10 @@
 //Config for liking tweets
-exports.keywords = ["#100DaysofCode"]; // array of hashtags to follow ["#100DaysofCode"]
-exports.blockedTerms = ["bot", "coupon"]; // array of terms that the tweet screen name should not have.
-exports.blockedTermsText = ["coupon", "discount", "hiring"];
-exports.maxFollower = 5000; //max follower user can have
-exports.maxRetweet = 10; //max retweet a tweet can have
-exports.maxFavorite = 10; // max likes a tweet can have
+exports.keywords = ["#Roblox"; "#RobloxDev"; "#RobloxEvebt"; "#RobloxArt"]; // array of hashtags to follow ["#100DaysofCode"]
+exports.blockedTerms = []; // array of terms that the tweet screen name should not have.
+exports.blockedTermsText = [];
+exports.maxFollower = 10000000000000000000; //max follower user can have
+exports.maxRetweet = 10000000000000000000; //max retweet a tweet can have
+exports.maxFavorite = 10000000000000000000; // max likes a tweet can have
 exports.accountsToFollow = [
-  "341643950", //ossia
-  "870038005", //traversymedia
-  "1069636036291112960", // developedbyed
 ]; // Retweet contents from these accounts
 // TODO add more accounts soon
